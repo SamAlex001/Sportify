97a23e835111150c7657cce7e902937710cfadd3
Sam Alex <sam1978al@gmail.com>
2024-01-06 13:36:29 +0530

    Login, SignUp Page UI Complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import './App.css'
+import { Routes, Route } from 'react-router-dom'
+import { Login } from './components/login'
+import { SignUp } from './components/signup'
 
 function App() {
   return (
-    <>
-    hello world
-    </>
+    <Routes>
+      <Route path='/login' element={<Login />} />
+      <Route path='/signup' element={<SignUp />} />
+    </Routes>
   )
 }
 
---
+++ src/components/login.jsx
@@ -0,0 +1,42 @@
+import { FcGoogle } from "react-icons/fc";
+import Logo from "../assets/Login_SignUP_Logo.png"
+import '../styles/login.css';
+import { useNavigate } from "react-router-dom";
+
+export const Login = () => {
+
+    const navigate = useNavigate();
+
+    return (
+        <div className='login-main-contianer'>
+            <div className="logo-container">
+                <img src={Logo} alt="Sportify_Logo" />
+            </div>
+            <div className="form-google-login-wrapper">
+                <form className='login-form-container' action="">
+                    <div className="username-wrapper">
+                        <div className="input-text prim--color">Username</div>
+                        <input type="text" className='custom-login-input' id='username' size={30} required />
+                    </div>
+                    <br />
+                    <div className="password-wrapper">
+                        <div className="input-text prim--color">Password</div>
+                        <input type="password" className='custom-login-input' id='password' size={30} required />
+                    </div>
+                    <br />
+                    <input type="submit" value="Login" className='login-form-btn' />
+                </form>
+                <h2 className="tert--color">OR</h2>
+                <div className="google-login-container">
+                    <button className='google-login-btn'>
+                        <FcGoogle className="login-icon" />
+                        login with google
+                    </button>
+                </div>
+                <button className="login-ref-link-container tert--color" onClick={() => navigate("/signup")}>
+                    don't have an account? click here to sign up
+                </button>
+            </div>
+        </div>
+    )
+}
